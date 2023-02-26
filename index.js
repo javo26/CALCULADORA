f43071d619cda63fb1d66827fe29f92cf48f5aea
javo26 <javier.marroqui26@gmail.com>
2023-02-26 00:50:56 -0600

    reinicioinicio de acciones de botones

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 var botonUno = document.getElementById('botonUno')
-var botonDos = document.getElementById('botonDos')
-var botonTres = document.getElementById('botonTres')
-var botonCuatro = document.getElementById('botonCuatro')
-var botonCinco = document.getElementById('botonCinco')
-var botonSeis = document.getElementById('botonSeis')
-var botonSiete = document.getElementById('botonSiete')
-var botonOcho = document.getElementById('botonOcho')
-var botonNueve = document.getElementById('botonNueve')
-var botonCero = document.getElementById('botonCero')
 var actual = document.getElementById('actual')
 var resultado = document.getElementById('resultado')
 
 botonUno.addEventListener('click',clickUno)
-botonDos.addEventListener('click',clickDos)
-botonTres.addEventListener('click',clickTres)
-botonCuatro.addEventListener('click',clickCuatro)
-botonCinco.addEventListener('click',clickCinco)
-botonSeis.addEventListener('click',clickSeis)
-botonSiete.addEventListener('click',clickSiete)
-botonOcho.addEventListener('click',clickOcho)
-botonNueve.addEventListener('click',clickNueve)
-botonCero.addEventListenner('click',clickCero)
-botonSuma.addEventListener('click',clickSuma)
-
 
 
 function clickUno(){actual.innerHTML+='1'}
-function clickDos(){actual.innerHTML+='2'}
-function clickTres(){actual.innerHTML+='3'}
-function clickCuatro(){actual.innerHTML+='4'}
-function clickCinco(){actual.innerHTML+='5'}
-function clickSeis(){actual.innerHTML+='6'}
-function clickSiete(){actual.innerHTML+='7'}
-function clickOcho(){actual.innerHTML+='8'}
-function clickNueve(){actual.innerHTML+='9'}
-function clickCero(){actual.innerHTML+='0'}
 
+/*
 function clickSuma(){
     let valorActual = parseInt(actual.innerHTML)
     let valorResultado = parseInt(resultado.innerHTML)
     resultado.innerHTML = valorResultado + valorActual
     actual.innerHTML = '0'
-}
+}*/
 
