2f9b0bbcf7a32e577a7d7ad957977ecd18c15ac5
javo26 <javier.marroqui26@gmail.com>
2023-02-26 01:02:59 -0600

    funcionamiento de botones

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+
 var botonUno = document.getElementById('botonUno')
+var botonDos = document.getElementById('botonDos')
+var botonSuma = document.getElementById('botonSuma')
 var actual = document.getElementById('actual')
 var resultado = document.getElementById('resultado')
 
 botonUno.addEventListener('click',clickUno)
+botonDos.addEventListener('click',clickDos)
+botonSuma.addEventListener('click',clickSuma)
 
 
 function clickUno(){actual.innerHTML+='1'}
+function clickDos(){actual.innerHTML+='2'}
 
-/*
 function clickSuma(){
     let valorActual = parseInt(actual.innerHTML)
     let valorResultado = parseInt(resultado.innerHTML)
-    resultado.innerHTML = valorResultado + valorActual
+    resultado.innerHTML = valorActual + valorResultado
     actual.innerHTML = '0'
-}*/
+}
 
